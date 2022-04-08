85df73019a4476feac155c4039f278173d47f728
Ingbian2020 <ingbianbenjamin@gmail.com>
2022-04-07 19:40:54 -0700

    my c programming4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
+ *main - entry point
  *
- * Return: 0 Always 0 (Success)
+ *Return: 0 after printing the function 
  */
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
-    return (0);
+return (0);
 }
 
