377f7dc1d669da8f20e8c80d30d6252b34054560
Ingbian2020 <ingbianbenjamin@gmail.com>
2022-04-13 11:39:30 -0700

    my c function1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
+ * print_alphabet - print alphabet lowercase
  *
- *  * print_alphabet - print alphabet lowercase
- *
- *   *
- *
- *    * Return: Always 0.
- *
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
